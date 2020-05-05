efdccab97c5e286daa19829ccb0d19a1465da320
Joshua Stoll <jds21171@uga.edu>
2020-05-05 13:06:32 -0400

    added inquirer

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
+var cli = require('./CLI')
+
+
 
 fs.writeFile("README.md", process.argv[2], function(err) {
     if (err) {
         console.log(err);
+    } else {
+        console.log("Success!");
     }
-    console.log("Success!");
-}) 
+});
